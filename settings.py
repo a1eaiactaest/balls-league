f59219a3cf2d19da7c83842a97752566c2938536
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-04-25 23:11:58 +0200

    game: class skeleton, draw screen

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+  def __init__(self):
+    # Screen
+    self.screen_width = 700
+    self.screen_height = 1000
+    self.screen_size = (self.screen_width, self.screen_height)
+
+    # Rocket 
+    self.rocket_speed = 5
+
